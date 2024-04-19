12765d957568bcbcc1ac1136c184641023b65800
Jongwon Park <jwparktom@gmail.com>
2024-04-19 14:06:15 -0500

    feat: More wrapper fns and natspec commentswith interface

--- contracts/StoryProtocolGateway.sol
+++ contracts/StoryProtocolGateway.sol
@@ -57,17 +57,28 @@ contract StoryProtocolGateway is IStoryProtocolGateway, AccessControlled, Access
         _disableInitializers();
     }
 
+    /// @dev Initializes the contract.
+    /// @param accessManager The address of the protocol access manager.
     function initialize(address accessManager) external initializer {
         if (accessManager == address(0)) revert Errors.SPG__ZeroAddressParam();
         __AccessManaged_init(accessManager);
         __UUPSUpgradeable_init();
     }
 
+    /// @dev Upgrades the NFT contract beacon. Restricted to only the protocol access manager.
+    /// @param newNftContract The address of the new NFT contract implemenetation.
     function upgradeCollections(address newNftContract) public restricted {
         // UpgradeableBeacon checks for newImplementation.bytecode.length > 0, so no need to check for zero address.
         UpgradeableBeacon(NFT_CONTRACT_BEACON).upgradeTo(newNftContract);
     }
 
+    /// @notice Creates a new NFT collection to be used by SPG.
+    /// @param name The name of the collection.
+    /// @param symbol The symbol of the collection.
+    /// @param maxSupply The maximum supply of the collection.
+    /// @param mintCost The cost to mint an NFT from the collection.
+    /// @param owner The owner of the collection.
+    /// @return nftContract The address of the newly created NFT collection.
     function createCollection(
         string memory name,
         string memory symbol,
@@ -79,18 +90,31 @@ contract StoryProtocolGateway is IStoryProtocolGateway, AccessControlled, Access
         ISPGNFT(nftContract).initialize(name, symbol, maxSupply, mintCost, owner);
     }
 
+    /// @notice Mint an NFT from a collection and register it as an IP.
+    /// @param nftContract The address of the NFT collection.
+    /// @param recipient The address of the recipient of the minted NFT.
+    /// @return tokenId The ID of the minted NFT.
+    /// @return ipId The ID of the registered IP.
     function mintAndRegisterIp(address nftContract, address recipient) public returns (uint256 tokenId, address ipId) {
         tokenId = ISPGNFT(nftContract).mint(recipient);
         ipId = IP_ASSET_REGISTRY.register(block.chainid, nftContract, tokenId);
     }
 
+    /// @notice Mint an NFT from a collection and register it with metadata as an IP.
+    /// @param nftContract The address of the NFT collection.
+    /// @param recipient The address of the recipient of the minted NFT.
+    /// @param metadataURI The URI of the metadata for the IP.
+    /// @param metadataHash The hash of the metadata for the IP.
+    /// @param nftMetadataHash The hash of the metadata for the IP NFT.
+    /// @return ipId The ID of the registered IP.
+    /// @return tokenId The ID of the minted NFT.
     function mintAndRegisterIp(
         address nftContract,
         address recipient,
         string memory metadataURI,
         bytes32 metadataHash,
         bytes32 nftMetadataHash
-    ) public returns (address ipId, uint256 tokenId) {
+    ) external returns (address ipId, uint256 tokenId) {
         tokenId = ISPGNFT(nftContract).mint(address(this));
         ipId = IP_ASSET_REGISTRY.register(block.chainid, nftContract, tokenId);
 
@@ -99,18 +123,30 @@ contract StoryProtocolGateway is IStoryProtocolGateway, AccessControlled, Access
         ISPGNFT(nftContract).safeTransferFrom(address(this), recipient, tokenId, "");
     }
 
+    /// @notice Register Programmable IP License Terms (if unregistered) and attach it to IP.
+    /// @param ipId The ID of the IP.
+    /// @param terms The PIL terms to be registered.
+    /// @return licenseTermsId The ID of the registered PIL terms.
     function registerPILTermsAndAttach(
         address ipId,
         PILTerms memory terms
-    ) public verifyPermission(ipId) returns (uint256 licenseTermsId) {
+    ) external verifyPermission(ipId) returns (uint256 licenseTermsId) {
         licenseTermsId = _registerPILTermsAndAttach(ipId, terms);
     }
 
+    /// @notice Mint an NFT from a collection, register it as an IP, register Programmable IP License Terms (if
+    /// unregistered), and attach it to the registered IP.
+    /// @param nftContract The address of the NFT collection.
+    /// @param recipient The address of the recipient of the minted NFT.
+    /// @param terms The PIL terms to be registered.
+    /// @return ipId The ID of the registered IP.
+    /// @return tokenId The ID of the minted NFT.
+    /// @return licenseTermsId The ID of the registered PIL terms.
     function mintAndRegisterIpAndAttachPILTerms(
         address nftContract,
         address recipient,
         PILTerms memory terms
-    ) public returns (address ipId, uint256 tokenId, uint256 licenseTermsId) {
+    ) external returns (address ipId, uint256 tokenId, uint256 licenseTermsId) {
         tokenId = ISPGNFT(nftContract).mint(address(this));
         ipId = IP_ASSET_REGISTRY.register(block.chainid, nftContract, tokenId);
 
@@ -119,6 +155,17 @@ contract StoryProtocolGateway is IStoryProtocolGateway, AccessControlled, Access
         ISPGNFT(nftContract).safeTransferFrom(address(this), recipient, tokenId, "");
     }
 
+    /// @notice Mint an NFT from a collection, register it with metadata as an IP, register Programmable IP License
+    /// Terms (if unregistered), and attach it to the registered IP.
+    /// @param nftContract The address of the NFT collection.
+    /// @param recipient The address of the recipient of the minted NFT.
+    /// @param metadataURI The URI of the metadata for the IP.
+    /// @param metadataHash The hash of the metadata for the IP.
+    /// @param nftMetadataHash The hash of the metadata for the IP NFT.
+    /// @param terms The PIL terms to be registered.
+    /// @return ipId The ID of the registered IP.
+    /// @return tokenId The ID of the minted NFT.
+    /// @return licenseTermsId The ID of the registered PIL terms.
     function mintAndRegisterIpAndAttachPILTerms(
         address nftContract,
         address recipient,
@@ -126,7 +173,7 @@ contract StoryProtocolGateway is IStoryProtocolGateway, AccessControlled, Access
         bytes32 metadataHash,
         bytes32 nftMetadataHash,
         PILTerms memory terms
-    ) public returns (address ipId, uint256 tokenId, uint256 licenseTermsId) {
+    ) external returns (address ipId, uint256 tokenId, uint256 licenseTermsId) {
         tokenId = ISPGNFT(nftContract).mint(address(this));
         ipId = IP_ASSET_REGISTRY.register(block.chainid, nftContract, tokenId);
 
@@ -137,17 +184,52 @@ contract StoryProtocolGateway is IStoryProtocolGateway, AccessControlled, Access
         ISPGNFT(nftContract).safeTransferFrom(address(this), recipient, tokenId, "");
     }
 
+    /// @notice Register a given NFT as an IP and attach Programmable IP License Terms.
+    /// @param nftContract The address of the NFT collection.
+    /// @param tokenId The ID of the NFT.
+    /// @param terms The PIL terms to be registered.
+    /// @return ipId The ID of the registered IP.
+    /// @return licenseTermsId The ID of the registered PIL terms.
     function registerIpAndAttachPILTerms(
         address nftContract,
         uint256 tokenId,
         PILTerms memory terms
-    ) public returns (address ipId, uint256 licenseTermsId) {
+    ) external returns (address ipId, uint256 licenseTermsId) {
         ipId = IP_ASSET_REGISTRY.register(block.chainid, nftContract, tokenId);
         _verifyPermission(ipId);
 
         licenseTermsId = _registerPILTermsAndAttach(ipId, terms);
     }
 
+    /// @notice Register a given NFT as an IP with metadata and attach Programmable IP License Terms.
+    /// @param nftContract The address of the NFT collection.
+    /// @param tokenId The ID of the NFT.
+    /// @param metadataURI The URI of the metadata for the IP.
+    /// @param metadataHash The hash of the metadata for the IP.
+    /// @param nftMetadataHash The hash of the metadata for the IP NFT.
+    /// @param terms The PIL terms to be registered.
+    /// @return ipId The ID of the registered IP.
+    /// @return licenseTermsId The ID of the registered PIL terms.
+    function registerIpAndAttachPILTerms(
+        address nftContract,
+        uint256 tokenId,
+        string memory metadataURI,
+        bytes32 metadataHash,
+        bytes32 nftMetadataHash,
+        PILTerms memory terms
+    ) external returns (address ipId, uint256 licenseTermsId) {
+        ipId = IP_ASSET_REGISTRY.register(block.chainid, nftContract, tokenId);
+        _verifyPermission(ipId);
+
+        CORE_METADATA_MODULE.setAll(ipId, metadataURI, metadataHash, nftMetadataHash);
+
+        licenseTermsId = _registerPILTermsAndAttach(ipId, terms);
+    }
+
+    /// @dev Registers PIL License Terms and attaches them to the given IP.
+    /// @param ipId The ID of the IP.
+    /// @param terms The PIL terms to be registered.
+    /// @return licenseTermsId The ID of the registered PIL terms.
     function _registerPILTermsAndAttach(address ipId, PILTerms memory terms) internal returns (uint256 licenseTermsId) {
         licenseTermsId = PIL_TEMPLATE.registerLicenseTerms(terms);
         LICENSING_MODULE.attachLicenseTerms(ipId, address(PIL_TEMPLATE), licenseTermsId);
--- contracts/interfaces/IStoryProtocolGateway.sol
+++ contracts/interfaces/IStoryProtocolGateway.sol
@@ -1,4 +1,114 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.23;
 
-interface IStoryProtocolGateway {}
+import { PILTerms } from "@storyprotocol/core/interfaces/modules/licensing/IPILicenseTemplate.sol";
+
+interface IStoryProtocolGateway {
+    /// @notice Creates a new NFT collection to be used by SPG.
+    /// @param name The name of the collection.
+    /// @param symbol The symbol of the collection.
+    /// @param maxSupply The maximum supply of the collection.
+    /// @param mintCost The cost to mint an NFT from the collection.
+    /// @param owner The owner of the collection.
+    /// @return nftContract The address of the newly created NFT collection.
+    function createCollection(
+        string memory name,
+        string memory symbol,
+        uint32 maxSupply,
+        uint256 mintCost,
+        address owner
+    ) external returns (address nftContract);
+
+    /// @notice Mint an NFT from a collection and register it as an IP.
+    /// @param nftContract The address of the NFT collection.
+    /// @param recipient The address of the recipient of the minted NFT.
+    /// @return tokenId The ID of the minted NFT.
+    /// @return ipId The ID of the registered IP.
+    function mintAndRegisterIp(address nftContract, address recipient) external returns (uint256 tokenId, address ipId);
+
+    /// @notice Mint an NFT from a collection and register it with metadata as an IP.
+    /// @param nftContract The address of the NFT collection.
+    /// @param recipient The address of the recipient of the minted NFT.
+    /// @param metadataURI The URI of the metadata for the IP.
+    /// @param metadataHash The hash of the metadata for the IP.
+    /// @param nftMetadataHash The hash of the metadata for the IP NFT.
+    /// @return ipId The ID of the registered IP.
+    /// @return tokenId The ID of the minted NFT.
+    function mintAndRegisterIp(
+        address nftContract,
+        address recipient,
+        string memory metadataURI,
+        bytes32 metadataHash,
+        bytes32 nftMetadataHash
+    ) external returns (address ipId, uint256 tokenId);
+
+    /// @notice Register Programmable IP License Terms (if unregistered) and attach it to IP.
+    /// @param ipId The ID of the IP.
+    /// @param terms The PIL terms to be registered.
+    /// @return licenseTermsId The ID of the registered PIL terms.
+    function registerPILTermsAndAttach(address ipId, PILTerms memory terms) external returns (uint256 licenseTermsId);
+
+    /// @notice Mint an NFT from a collection, register it as an IP, register Programmable IP License Terms (if
+    /// unregistered), and attach it to the registered IP.
+    /// @param nftContract The address of the NFT collection.
+    /// @param recipient The address of the recipient of the minted NFT.
+    /// @param terms The PIL terms to be registered.
+    /// @return ipId The ID of the registered IP.
+    /// @return tokenId The ID of the minted NFT.
+    /// @return licenseTermsId The ID of the registered PIL terms.
+    function mintAndRegisterIpAndAttachPILTerms(
+        address nftContract,
+        address recipient,
+        PILTerms memory terms
+    ) external returns (address ipId, uint256 tokenId, uint256 licenseTermsId);
+
+    /// @notice Mint an NFT from a collection, register it with metadata as an IP, register Programmable IP License
+    /// Terms (if unregistered), and attach it to the registered IP.
+    /// @param nftContract The address of the NFT collection.
+    /// @param recipient The address of the recipient of the minted NFT.
+    /// @param metadataURI The URI of the metadata for the IP.
+    /// @param metadataHash The hash of the metadata for the IP.
+    /// @param nftMetadataHash The hash of the metadata for the IP NFT.
+    /// @param terms The PIL terms to be registered.
+    /// @return ipId The ID of the registered IP.
+    /// @return tokenId The ID of the minted NFT.
+    /// @return licenseTermsId The ID of the registered PIL terms.
+    function mintAndRegisterIpAndAttachPILTerms(
+        address nftContract,
+        address recipient,
+        string memory metadataURI,
+        bytes32 metadataHash,
+        bytes32 nftMetadataHash,
+        PILTerms memory terms
+    ) external returns (address ipId, uint256 tokenId, uint256 licenseTermsId);
+
+    /// @notice Register a given NFT as an IP and attach Programmable IP License Terms.
+    /// @param nftContract The address of the NFT collection.
+    /// @param tokenId The ID of the NFT.
+    /// @param terms The PIL terms to be registered.
+    /// @return ipId The ID of the registered IP.
+    /// @return licenseTermsId The ID of the registered PIL terms.
+    function registerIpAndAttachPILTerms(
+        address nftContract,
+        uint256 tokenId,
+        PILTerms memory terms
+    ) external returns (address ipId, uint256 licenseTermsId);
+
+    /// @notice Register a given NFT as an IP with metadata and attach Programmable IP License Terms.
+    /// @param nftContract The address of the NFT collection.
+    /// @param tokenId The ID of the NFT.
+    /// @param metadataURI The URI of the metadata for the IP.
+    /// @param metadataHash The hash of the metadata for the IP.
+    /// @param nftMetadataHash The hash of the metadata for the IP NFT.
+    /// @param terms The PIL terms to be registered.
+    /// @return ipId The ID of the registered IP.
+    /// @return licenseTermsId The ID of the registered PIL terms.
+    function registerIpAndAttachPILTerms(
+        address nftContract,
+        uint256 tokenId,
+        string memory metadataURI,
+        bytes32 metadataHash,
+        bytes32 nftMetadataHash,
+        PILTerms memory terms
+    ) external returns (address ipId, uint256 licenseTermsId);
+}
