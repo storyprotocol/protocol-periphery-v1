f7fa8d67393af50504aba34220df201978a080fb
Jongwon Park <jwparktom@gmail.com>
2024-04-19 11:12:14 -0500

    refactor: remove unused tests & add proxy helper

---
+++ test/utils/TestProxyHelper.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.23;
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+library TestProxyHelper {
+    /// Deploys a new UUPS proxy with the provided implementation and data
+    /// @dev WARNING: DO NOT USE IN PRODUCTION, this doesn't check for storage layout compatibility
+    /// @param impl address of the implementation contract
+    /// @param data encoded initializer call
+    function deployUUPSProxy(address impl, bytes memory data) internal returns (address) {
+        ERC1967Proxy proxy = new ERC1967Proxy(impl, data);
+        return address(proxy);
+    }
+}
