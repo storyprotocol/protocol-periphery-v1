88235f7cc87324d8a947d651c24549321a2bbd5d
Jongwon Park <contact@parkjongwon.com>
2024-05-14 00:54:16 -0700

    fix: salt seed

--- script/UpgradeSPGNFT.s.sol
+++ script/UpgradeSPGNFT.s.sol
@@ -20,7 +20,7 @@ contract UpgradeSPGNFT is Script, StoryProtocolPeripheryAddressManager, Broadcas
     using StringUtil for uint256;
 
     ICreate3Deployer private constant create3Deployer = ICreate3Deployer(0x384a891dFDE8180b054f04D66379f16B7a678Ad6);
-    uint256 private constant create3SaltSeed = 13;
+    uint256 private constant create3SaltSeed = 15;
 
     StoryProtocolGateway private spg;
     SPGNFT private spgNftImpl;
