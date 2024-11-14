da5bcc9996e4857ff154575d0094ea1d557d45e2
Sebastian Liu <sebsadface@gmail.com>
2024-11-13 17:54:06 -0800

    chore(royalty workflows): update comments

--- contracts/workflows/RoyaltyWorkflows.sol
+++ contracts/workflows/RoyaltyWorkflows.sol
@@ -48,10 +48,13 @@ contract RoyaltyWorkflows is IRoyaltyWorkflows, MulticallUpgradeable, AccessMana
     /// @param ancestorIpId The address of the ancestor IP from which the revenue is being claimed.
     /// @param claimer The address of the claimer of the currency (revenue) tokens.
     /// @param childIpIds The addresses of the child IPs from which royalties are derived.
-    /// @param royaltyPolicies The addresses of the royalty policies that govern royalty flows for each child IP.
-    /// @param currencyTokens The addresses of the currency tokens in which the royalties will be claimed.
-    /// @param amounts The amounts (in each currency) of royalties to be transferred to the ancestor IP's
-    ///        royalty vault and subsequently claimed by the claimer.
+    /// @param royaltyPolicies The addresses of the royalty policies, where royaltyPolicies[i] governs
+    ///        the royalty flow for childIpIds[i].
+    /// @param currencyTokens The addresses of the currency tokens in which royalties will be claimed,
+    ///        where currencyTokens[i] is the token used for royalties from childIpIds[i].
+    /// @param amounts The amounts to transfer and claim, where amounts[i] represents the amount of
+    ///        royalties in currencyTokens[i] to transfer from childIpIds[i]'s royaltyPolicies[i] to the ancestor's
+    ///        royalty vault.
     /// @return amountsClaimed The amounts of successfully claimed revenue for each specified currency token.
     function transferToVaultAndClaimByTokenBatch(
         address ancestorIpId,
@@ -87,8 +90,10 @@ contract RoyaltyWorkflows is IRoyaltyWorkflows, MulticallUpgradeable, AccessMana
     /// @param ancestorIpId The address of the ancestor IP from which the revenue is being claimed.
     /// @param claimer The address of the claimer of the currency (revenue) tokens.
     /// @param childIpIds The addresses of the child IPs from which royalties are derived.
-    /// @param royaltyPolicies The addresses of the royalty policies that govern royalty flows for each child IP.
-    /// @param currencyTokens The addresses of the currency tokens in which the royalties will be claimed.
+    /// @param royaltyPolicies The addresses of the royalty policies, where royaltyPolicies[i] governs
+    ///        the royalty flow for childIpIds[i].
+    /// @param currencyTokens The addresses of the currency tokens in which royalties will be claimed,
+    ///        where currencyTokens[i] is the token used for royalties from childIpIds[i].
     /// @return amountsClaimed The amounts of successfully claimed revenue for each specified currency token.
     function claimAllRevenue(
         address ancestorIpId,
