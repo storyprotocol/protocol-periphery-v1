109e0c642b636c8222c06bca3ef5c70206223154
Jacqueline Zhang <jacqueline@57blocks.com>
2025-04-18 10:39:15 +0800

    update test function and remove helper

--- test/integration/workflows/SpgNftIntegration.t.sol
+++ test/integration/workflows/SpgNftIntegration.t.sol
@@ -20,15 +20,8 @@ import { BaseIntegration } from "../BaseIntegration.t.sol";
  * @notice Integration test for SPGNFT with focus on token URI functionality
  */
 contract SpgNftIntegration is BaseIntegration {
-    using Strings for uint256;
-
-    // Constants
     string private constant CUSTOM_TOKEN_URI = "test-token-uri";
-    bytes32 private constant CUSTOM_IP_HASH = bytes32("custom-ip-hash");
-    bytes32 private constant CUSTOM_NFT_HASH = bytes32("custom-nft-hash");
-    string private constant CUSTOM_IP_URI = "custom-ip-uri";
 
-    // Contract instance
     ISPGNFT private spgNftContract;
 
     /// @dev To use, run the following command:
@@ -38,12 +31,43 @@ contract SpgNftIntegration is BaseIntegration {
         super.run();
         _beginBroadcast();
 
-        // Create NFT collection with empty baseURI to test setTokenURI more effectively
+        // Create SPG NFT collection
         spgNftContract = _createNftCollection();
 
         // Run the test
         _test_SpgNftIntegration_setTokenURI();
         _endBroadcast();
+    }
+
+    /**
+     * @dev Test function to verify the setTokenURI functionality
+     */
+    function _test_SpgNftIntegration_setTokenURI() private logTest("test_SpgNftIntegration_setTokenURI") {
+        wrappedIP.deposit{ value: testMintFee }();
+        wrappedIP.approve(address(spgNftContract), testMintFee);
+
+        // Mint and register IP
+        (address ipId, uint256 tokenId) = registrationWorkflows.mintAndRegisterIp({
+            spgNftContract: address(spgNftContract),
+            recipient: testSender,
+            ipMetadata: testIpMetadata,
+            allowDuplicates: true
+        });
+
+        // Get the initial URI for comparison
+        string memory initialUri = spgNftContract.tokenURI(tokenId);
+        console2.log("Initial URI:", initialUri);
+
+        assertEq(initialUri, string.concat(testBaseURI, testIpMetadata.nftMetadataURI));
+
+        // Update the token URI
+        spgNftContract.setTokenURI(tokenId, CUSTOM_TOKEN_URI);
+
+        // Get and verify the updated URI
+        string memory updatedUri = spgNftContract.tokenURI(tokenId);
+        console2.log("Updated token URI:", updatedUri);
+
+        assertEq(updatedUri, string.concat(testBaseURI, CUSTOM_TOKEN_URI));
     }
 
     /**
@@ -65,74 +89,9 @@ contract SpgNftIntegration is BaseIntegration {
                         mintFeeRecipient: testSender,
                         owner: testSender,
                         mintOpen: true,
-                        isPublicMinting: true // Public minting enabled for easy testing
+                        isPublicMinting: true
                     })
                 )
             );
-    }
-
-    /**
-     * @dev Test function to verify the setTokenURI functionality
-     * The test mints an NFT with empty nftMetadataURI and then sets a custom token URI
-     */
-    function _test_SpgNftIntegration_setTokenURI()
-        private
-        logTest("test_SpgNftIntegration_setTokenURI")
-    {
-        // Prepare token for minting
-        _prepareForMinting();
-
-        // Mint and register IP
-        (address ipId, uint256 tokenId) = _mintAndRegisterIp(testIpMetadata);
-
-        // Set and verify custom token URI
-        _setAndVerifyTokenURI(tokenId);
-    }
-
-    /**
-     * @dev Prepare for minting by depositing and approving tokens
-     */
-    function _prepareForMinting() private {
-        wrappedIP.deposit{ value: testMintFee }();
-        wrappedIP.approve(address(spgNftContract), testMintFee);
-    }
-
-    /**
-     * @dev Mint and register IP using the provided metadata
-     * @param ipMetadata Metadata to use for the new IP
-     * @return ipId Address of the registered IP
-     * @return tokenId ID of the minted token
-     */
-    function _mintAndRegisterIp(
-        WorkflowStructs.IPMetadata memory ipMetadata
-    ) private returns (address ipId, uint256 tokenId) {
-        return
-            registrationWorkflows.mintAndRegisterIp({
-                spgNftContract: address(spgNftContract),
-                recipient: testSender,
-                ipMetadata: ipMetadata,
-                allowDuplicates: true
-            });
-    }
-
-    /**
-     * @dev Set and verify a custom token URI
-     * @param tokenId The token ID to update
-     */
-    function _setAndVerifyTokenURI(uint256 tokenId) private {
-        // Get the initial URI for comparison
-        string memory initialUri = spgNftContract.tokenURI(tokenId);
-        console2.log("Initial URI:", initialUri);
-
-        assertEq(initialUri, string.concat(testBaseURI, testIpMetadata.nftMetadataURI));
-
-        // Update the token URI
-        spgNftContract.setTokenURI(tokenId, CUSTOM_TOKEN_URI);
-
-        // Get and verify the updated URI
-        string memory updatedUri = spgNftContract.tokenURI(tokenId);
-        console2.log("Updated token URI:", updatedUri);
-
-        assertEq(updatedUri, string.concat(testBaseURI, CUSTOM_TOKEN_URI));
     }
 }
